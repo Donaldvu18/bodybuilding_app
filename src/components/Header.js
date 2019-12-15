141adb86e07c6088d88cb6be61e522cbded5f043
Don Vu <dvu37@gatech.edu>
2019-12-15 01:51:52 -0800

    make "Don Fitness Plan" italicized

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return(
         <div className='header-don'>
-        <h1 className='text-center pt-3'>Ready for the Don Fitness Plan?</h1> 
+        <h1 className='text-center pt-3'>Ready for the <span>Don Fitness Plan?</span></h1> 
         <p className='text-center'>Select a body part and then scroll down!  <br/>You may also search for a specific muscle!</p>
         </div>
     );
