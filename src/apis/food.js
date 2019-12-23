a1d44b132ce2f93d1303beebbc1c0e281152c0f5
Don Vu <dvu37@gatech.edu>
2019-12-23 15:38:26 -0800

    fix api

--- src/apis/food.js
+++ src/apis/food.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.spoonacular.com/recipes',
     params:{
-        apiKey: REACT_APP_FOOD_API_KEY,
+        apiKey: process.env.REACT_APP_FOOD_API_KEY,
         number:6
     }
 })
