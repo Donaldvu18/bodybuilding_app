dc0300aaac1900ffed6a144eab1fc0bfcb013f4c
Don Vu <dvu37@gatech.edu>
2019-12-23 16:21:37 -0800

    add copyright at bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return(
-    <div className='page-footer align-items-end'>Created by Donald Vu</div>
+    <div className='page-footer align-items-end'>Created by Donald Vu &copy; 2019</div>
     );
 };
 
